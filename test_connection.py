5cb970b8bc8baa3603c2dd9d0ee01046dc11f479
Ivan Gorbachev <ip0000h@gmail.com>
2020-06-08 23:38:56 +0200

    test scripts changed

--- test_connection.py
+++ test_connection.py
@@ -1,12 +1,14 @@
-import time
 from ibapi.client import EClient
 from ibapi.wrapper import EWrapper
 
 
 class IBapi(EWrapper, EClient):
+
     def __init__(self):
+        EWrapper.__init__(self)
         EClient.__init__(self, self)
 
+
 app = IBapi()
-app.connect('127.0.0.1', 7497, 123)
+app.connect('127.0.0.1', 7497, 1)
 app.run()
--- test_latest_price.py
+++ test_latest_price.py
@@ -13,10 +13,7 @@ class IBapi(EWrapper, EClient):
         EClient.__init__(self, self)
 
     def tickPrice(self, reqId, tickType, price, attrib):
-        if reqId == 1:
-            print(f'The current USD_EUR({reqId}) tickType:{tickType} price is: {price}')
-        elif reqId == 2:
-            print(f'The current BTC({reqId}) tickType:{tickType} price is: {price}')
+        print(f'The current USD_EUR({reqId}) tickType:{tickType} price is: {price}')
 
 
 def run_loop():
@@ -24,37 +21,20 @@ def run_loop():
 
 
 app = IBapi()
-app.connect('127.0.0.1', 7497, 123)
+app.connect('127.0.0.1', 7497, 1)
 
 # Start the socket in a thread
 api_thread = threading.Thread(target=run_loop, daemon=True)
 api_thread.start()
 
-time.sleep(1) # Sleep interval to allow time for connection to server
+time.sleep(1) #  Sleep interval to allow time for connection to server
 
-apple_contract = Contract()
-apple_contract.symbol = 'AAPL'
-apple_contract.secType = 'STK'
-apple_contract.exchange = 'SMART'
-apple_contract.currency = 'USD'
-
+# EUR\USD contract
 eurusd_contract = Contract()
 eurusd_contract.symbol = 'EUR'
 eurusd_contract.secType = 'CASH'
 eurusd_contract.exchange = 'IDEALPRO'
 eurusd_contract.currency = 'USD'
 
-btc_futures__contract = Contract()
-btc_futures__contract.symbol = 'BRR'
-btc_futures__contract.secType = 'FUT'
-btc_futures__contract.exchange = 'CMECRYPTO'
-btc_futures__contract.lastTradeDateOrContractMonth = '202006'
-
-# MarketDataTypeEnum.DELAYED
-# app.reqMarketDataType(3)
-
 # Request Market Data for EUR\USD
 app.reqMktData(1, eurusd_contract, '', False, False, [])
-
-# Request Market Data for BTC
-# app.reqMktData(2, btc_futures__contract, '', False, False, [])
